2f815435846e1bac01c607641e7fdc4743b2ad8b
20024377 <20024377-Toh-YuanWei>
2021-07-30 15:40:17 +0800

    Done By YuanWei

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -4,12 +4,46 @@ public class C206_CaseStudy {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		
+		
 		int option = 0;
 		
-		while(option != 5)
+		while(option != 6) //YuanWei
 		{
 			
+			option = Helper.readInt("Enter an option > ");
 			
+			if(option == 1)
+			{
+				//Add student
+			}
+			
+			else if(option == 2)
+			{
+				// Register for tuition
+			}
+			
+			else if(option == 3)
+			{
+				// Add equiry
+			}
+			
+			else if(option == 4)
+			{
+				// Add tuition time table
+				
+				
+			}
+			
+			else if(option == 5)
+			{
+				//Add tuition
+			}
+			
+			else if(option == 6)
+			{
+				System.out.println("Thank you for using the Tuition Management App !");
+			}
 		}
 
 	}
---
+++ C206_CaseStudy/src/tuitionTimetable.java
@@ -0,0 +1,79 @@
+import java.time.LocalDateTime;
+
+/**
+ * 
+ */
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism
+ *
+ * Toh Yuan Wei 20024377, 30 Jul 2021 3:37:38 pm
+ */
+public class tuitionTimetable {
+	
+	private int tuitionID;
+	private double price;
+	private LocalDateTime startDate;
+	private LocalDateTime endTime;
+	private String mode;
+
+	
+	public tuitionTimetable(int tuitionID, double price, LocalDateTime startDate, LocalDateTime endTime, String mode) {
+		super();
+		this.tuitionID = tuitionID;
+		this.price = price;
+		this.startDate = startDate;
+		this.endTime = endTime;
+		this.mode = mode;
+	}
+
+
+	/**
+	 * @return the tuitionID
+	 */
+	public int getTuitionID() {
+		return tuitionID;
+	}
+
+
+	/**
+	 * @return the price
+	 */
+	public double getPrice() {
+		return price;
+	}
+
+
+	/**
+	 * @return the startDate
+	 */
+	public LocalDateTime getStartDate() {
+		return startDate;
+	}
+
+
+	/**
+	 * @return the endTime
+	 */
+	public LocalDateTime getEndTime() {
+		return endTime;
+	}
+
+
+	/**
+	 * @return the mode
+	 */
+	public String getMode() {
+		return mode;
+	}
+	
+	
+	
+	
+
+
+
+
+}

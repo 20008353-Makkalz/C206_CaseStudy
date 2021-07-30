457df38dcc7bfd4c4b54773e422a543d3de086a2
20024377 <20024377-Toh-YuanWei>
2021-07-30 15:32:06 +0800

    Done by Yuan Wei

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -3,6 +3,14 @@ public class C206_CaseStudy {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int option = 0;
+		
+		while(option != 5)
+		{
+			
+			
+		}
 
 	}
 

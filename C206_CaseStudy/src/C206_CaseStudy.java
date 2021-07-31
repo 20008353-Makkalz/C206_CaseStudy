45ed41ead33cdaa1a5e5b7f1e4b7aef6c43c0ca3
20045485 <20045485@myrp.edu.sg>
2021-07-31 16:53:17 +0800

    Done by Gilbert

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,4 +1,6 @@
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 
 public class C206_CaseStudy {
@@ -6,9 +8,16 @@ public class C206_CaseStudy {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ArrayList<tuitionTimetable> timetableList = new ArrayList<tuitionTimetable>(); //YuanWei
-
+		//===Gilbert
+		ArrayList<Enquiry>enquiryList = new ArrayList<Enquiry>(); 
+		enquiryList.add(new Enquiry("How to register", "2021-7-20", "09.15", 
+				 "Email","Pending"));
 		
+		enquiryList.add(new Enquiry("Tuition Fee", "2021-7-5", "13.15",
+				 "Email","Completed"));
 		
+		//===Gilbert
+		//
 		
 		int option = 0;
 		
@@ -27,9 +36,42 @@ public class C206_CaseStudy {
 				// Register for tuition
 			}
 			
-			else if(option == 3)
+			else if(option == 3) ///Gilbert Ng -Option 3[Enquiry]
 			{
 				// Add enquiry
+				int option3 = 0;
+				
+				while(option != 4)
+				{
+					doOption3menu();
+					option3 = Helper.readInt("Enter an option > ");
+				
+					if(option3 == 1)
+					{
+						viewEnquiry(enquiryList);
+					}
+					else if(option3 == 2)// Delete Enquiry //Gilbert
+					{
+						
+					}
+					else if(option3 == 3)//Add Enquiry //Gilbert
+					{
+						
+						String title = Helper.readString("Enter Enquiry Title > ");
+						String date = Helper.readString("Enter data in (YYYY-MM-DD) > ");
+						String time = Helper.readString("Enter time in (TT.MM) > ");
+						String enquiryMethod = Helper.readString("Enter enquiry method > ");
+						String status = Helper.readString("Enter enquiry status > ");
+						enquiryList.add(new Enquiry (title, date, time,enquiryMethod, status));
+					}
+					else if(option3 == 4)
+					{
+						System.out.println("Returning to main menu....");
+				
+					}
+					
+				}
+				
 			}
 			
 			else if(option == 4)
@@ -104,6 +146,33 @@ public class C206_CaseStudy {
 		System.out.println("5. Tuition");
 		System.out.println("6. Quit");
 		
+	}
+	public static void doOption3menu() //Gilbert Ng
+	{
+		Helper.line(50, "=");
+		System.out.println("Tuition Enquiry");
+		Helper.line(50, "=");
+		
+
+		System.out.println("1. View Enquiry");
+		System.out.println("2. Delete Enquiry");
+		System.out.println("3. Add Enquiry");
+		System.out.println("4. Quit");
+	}
+	
+	public static void viewEnquiry(ArrayList<Enquiry> enquiryList) //Gilbert Ng
+	{
+		int count = 1;
+		String output = String.format("%-15s %-20s %-15s %-15s %-20s %-10s\n", "Enquiry_ID","Title","Date", "Time",
+				"Enquiry_Method","Status");
+		
+		for(Enquiry e : enquiryList)
+		{
+			output += String.format("%-15s %-20s %-15s %-15s %-20s %-10s\n", count ++,e.getTitle(),e.getDate(), e.getTime(),
+					 e.getEnquiryMethod(),e.getStatus());
+		}
+		
+		System.out.println(output);
 	}
 	
 	public static void doOption4menu() //YuanWei
---
+++ C206_CaseStudy/src/Enquiry.java
@@ -0,0 +1,62 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * Gilbert Ng 20045485, 30 Jul 2021 11:52:44 pm
+ */
+
+public class Enquiry {
+	
+	private int enquiryID;
+	private String title;
+	private String date;
+	private String time;
+	private String enquiryMethod;
+	private String status;
+
+	public Enquiry(String title, String date, String time, String enquiryMethod, String status) {
+		
+		this.enquiryID = 0;
+		this.title = title;
+		this.date = date;
+		this.time = time;
+		this.enquiryMethod = enquiryMethod;
+		this.status = status;
+	}
+
+	public int getEnquiryID() {
+		return enquiryID;
+	}
+
+	
+	public String getTitle() {
+		return title;
+	}
+
+	
+	public String getDate() {
+		return date;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public String getEnquiryMethod() {
+		return enquiryMethod;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+	
+	
+	
+	
+	
+}
